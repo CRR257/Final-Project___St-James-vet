537cde24f6ef19971c10ebb1495297a431b27031
lluna <lluna@example.com>
2019-03-24 13:09:04 +0100

    st-james-vet

---
+++ st-james-vet-app/build/precache-manifest.375b287f3ca2f05ea02cfc5f00c27d92.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6a7a1a00c7b87ec54b08881d4619600",
+    "url": "/static/media/tortoise.e6a7a1a0.jpg"
+  },
+  {
+    "revision": "f2d9285b739eed59bf31",
+    "url": "/static/css/main.6247da0c.chunk.css"
+  },
+  {
+    "revision": "5a180a4e1e0875c16418",
+    "url": "/static/js/1.5a180a4e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eff5b24625a272e4943e7c4fad387b1b",
+    "url": "/static/media/akita.eff5b246.jpg"
+  },
+  {
+    "revision": "69df44be77c2538e32e8aa26d0ab0d9b",
+    "url": "/static/media/boyero.69df44be.jpg"
+  },
+  {
+    "revision": "99f68d1f4a050d5089852c9137486f1c",
+    "url": "/static/media/british_cat.99f68d1f.jpg"
+  },
+  {
+    "revision": "f2d9285b739eed59bf31",
+    "url": "/static/js/main.f2d9285b.chunk.js"
+  },
+  {
+    "revision": "3b9abc877c0c757bf5377ef67dee0ef3",
+    "url": "/static/media/tortoise2.3b9abc87.jpg"
+  },
+  {
+    "revision": "edd2748ac0a40e5f78e44cb7af62fe73",
+    "url": "/static/media/tortoise3.edd2748a.jpg"
+  },
+  {
+    "revision": "5fe9cd77b05ecc595dfd38bd5b0a84c0",
+    "url": "/static/media/cat.5fe9cd77.jpg"
+  },
+  {
+    "revision": "f3a0115d3fa51768cc4abbb2b473dd14",
+    "url": "/static/media/british2.f3a0115d.jpg"
+  },
+  {
+    "revision": "7d8124fd6b664d59448d37c2579efb84",
+    "url": "/static/media/cat_dog.7d8124fd.png"
+  },
+  {
+    "revision": "f47a97ac7ecf58ac8f0396b219de13db",
+    "url": "/static/media/wood.f47a97ac.jpg"
+  },
+  {
+    "revision": "2e19e13a583d994ce935dac446e83a61",
+    "url": "/index.html"
+  }
+];
